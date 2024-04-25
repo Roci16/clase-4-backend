e31e0c02a4e12f79c26a74efb4e2f4775a9f66a3
ket-id <ket-id@outlook.com>
2024-04-24 21:54:15 -0300

    clase practica

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const http = require('');
+const { isCryptoKey } = require('util/types');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    let respuesta = "";
+    let statusCode = 200;
+
+    if (req.url === '/') {
+        respuesta = "Bienvenidos, Gracias por tu visita";
+    } else if (req.url === '/nosotras') {
+        respuesta = "Bienvenidos a saber + de nosotras :)";
+    } else if (req.url === '/cursos') {
+        respuesta = "Te interesan nuestros cursos?. Veni a conocernos";
+    } else if (req.url === '/contacto') {
+        respuesta = "Si te gusta contactanos";
+    } else {
+        statusCode = 404;
+        respuesta = "No se encontró la ruta";
+    }
+
+    res.statusCode = statusCode
+    res.setHeader('charset', 'utf-8')
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(respuesta)
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
